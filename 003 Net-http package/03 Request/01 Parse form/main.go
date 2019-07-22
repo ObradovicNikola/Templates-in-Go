90e8a2151f30f4c7cb2dad760a59b83f2dc23060
ObradovicNikola <obradovicnikola009@gmail.com>
2019-07-22 10:53:09 +0200

    Initial commit

---
+++ 003 Net-http package/03 Request/01 Parse form/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type hotdog int
+
+// ataching method to type hotdog
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tmpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+}
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
